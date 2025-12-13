b1475a077c485881d95d1ab9b3306e4b7a848397
prasadrprabhu77 <prasadrprabhu77@gmail.com>
2025-12-13 20:54:36 +0530

    add jwt auth middleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { login, signup } from "../controllers/authController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup);
-authRouter.post("/login", login)
+authRouter.post("/login", login);
+authRouter.get("/me", authMiddleware, (req, res) => {
+  res.json(req.user);
+});
 
 export default authRouter;
