859d0e7ac3f4c52b3399be3c30fb0b5db773dcd6
prasadrprabhu77 <prasadrprabhu77@gmail.com>
2025-12-13 19:44:40 +0530

    add login api with jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/User.js";
+import generateToken from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -29,6 +30,42 @@ export const signup = async (req, res) => {
     res.status(201).json({
       message: "User registered successfully",
       userId: user._id
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // validation
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // check user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      token: generateToken(user._id),
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/authController.js";
+import { login, signup } from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup);
+authRouter.post("/login", login)
 
 export default authRouter;
