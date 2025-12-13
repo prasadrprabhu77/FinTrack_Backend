758a71383f3a288c39e57679d78ff96132a5e063
prasadrprabhu77 <prasadrprabhu77@gmail.com>
2025-12-13 13:49:24 +0530

    setup basic express server

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
