02f3e27f90eba9eae0cdd82eb218b2e8b1a81068
prasadrprabhu77 <prasadrprabhu77@gmail.com>
2025-12-18 19:59:16 +0530

    add transaction model

---
+++ models/Transactions.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: true
+    },
+    amount: {
+      type: Number,
+      required: true
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    note: {
+      type: String,
+      trim: true
+    },
+    wallet: {
+      type: String,
+      default: "Cash"
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Transaction", transactionSchema);
